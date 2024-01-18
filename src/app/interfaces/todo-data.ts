38c040598c651b0e5c8b7342f238e625be71da18
Muhammd-Shah-zaib <muhammdshahzaib@outlook.com>
2024-01-18 15:03:07 +0500

    added an interface for getting the data

---
+++ src/app/interfaces/todo-data.ts
@@ -0,0 +1,7 @@
+export type Root = Root2[]
+
+export interface Root2 {
+    id: string
+    name?: string
+    task?: string
+}
--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,9 +1,18 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import { observableToBeFn } from 'rxjs/internal/testing/TestScheduler';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
+  // injecting HttpClient
+  http: HttpClient = inject(HttpClient);
 
   constructor() { }
+
+  getData(): Observable<any> {
+    return this.http.get("http://localhost:3000/tasks/");
+  }
 }
