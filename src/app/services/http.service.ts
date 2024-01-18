5aa7af8728048205b3ff6ab5e6609da36edd20c1
Muhammd-Shah-zaib <muhammdshahzaib@outlook.com>
2024-01-19 00:31:56 +0500

    adedd a postData function in service

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -12,11 +12,12 @@ export class HttpService {
   url: string = 'http://localhost:3000/tasks/'
   constructor() { }
 
-  // getData() method returns an Observable of TodoData)
+  // getData() method that is used to get all tasks from our db.json
   getData(): Observable<TodoData> {
     return this.http.get<TodoData>(this.url);
   }
 
+  // getDataById() method that is used to get specific task from our db.json
   getDataById( id: number ): Observable<TodoData> {
     return this.http.get<TodoData>(this.url + id);
   }
@@ -26,10 +27,13 @@ export class HttpService {
     return this.http.put<TodoData>(this.url + id, task);
   }
 
-
   // deleteData() method that is used to delete specific task in our db.json
   deleteData( id: number ): Observable<TodoData> {
     return this.http.delete<TodoData>(this.url + id);
   }
 
+  // postData() method that is used to add new task to our db.json
+  postData( task: Root2 ): Observable<TodoData> {
+    return this.http.post<TodoData>(this.url, task);
+  }
 }
