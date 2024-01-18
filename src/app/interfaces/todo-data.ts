be1cc45cd848bc76201171437c3ca494c69a3f8c
Muhammd-Shah-zaib <muhammdshahzaib@outlook.com>
2024-01-18 15:04:16 +0500

    updated the TodoData interface

--- src/app/interfaces/todo-data.ts
+++ src/app/interfaces/todo-data.ts
@@ -1,7 +1,9 @@
-export type Root = Root2[]
+export type TodoData = Root2[]
 
 export interface Root2 {
-    id: string
-    name?: string
-    task?: string
+    id: string;
+    name?: string;
+    task?: string;
+    completed?: boolean;
+    important?: boolean;
 }
