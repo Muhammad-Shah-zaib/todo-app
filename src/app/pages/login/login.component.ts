1bfdcd087585391c218bdb22af456c8c662ee688
Muhammd-Shah-zaib <muhammdshahzaib@outlook.com>
2024-01-20 18:51:09 +0500

    finalized login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -5,6 +5,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
+import { UserValidationService } from '../../services/user-validation.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -18,14 +20,20 @@ export class LoginComponent implements OnInit {
   // making a property for checking particular element is focused or not
   isFocused: boolean = false;
 
+  private validateService: UserValidationService = new UserValidationService();
+  private router: Router = new Router();
+
   // Making a formGroup 
   userForm = new FormGroup({
     // making username FormControl
     username: new FormControl<string>('', [Validators.required, Validators.pattern('^[a-zA-Z][_a-zA-Z0-9]*$'), Validators.minLength(5), Validators.maxLength(25) ]),
 
     // makking password FormControl
-    password: new FormControl<string>('', [Validators.required, Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[_+\\-@$!%*#?&]).+$'), Validators.minLength(8)])
+    // password: new FormControl<string>('', [Validators.required, Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[_+\\-@$!%*#?&]).+$'), Validators.minLength(8)])
+    password: new FormControl<string>('', [Validators.required, Validators.minLength(8)])
+    
   })
+
 
 
   constructor() {}
@@ -44,5 +52,14 @@ export class LoginComponent implements OnInit {
 
   onSubmit(): void {
     console.log(this.userForm.value);
+    if ((this.userForm.value.username !== undefined && this.userForm.value.username !== null )
+      && (this.userForm.value.password !== undefined && this.userForm.value.password !== null)){
+    
+        if (this.validateService.checkLoginCredentials( this.userForm.value.username, this.userForm.value.password)) {
+          this.router.navigate(['/home']);
+        }else {
+          alert('Incorrect')
+        }
+      }
   }
 }
--- src/app/services/user-validation.service.ts
+++ src/app/services/user-validation.service.ts
@@ -1,9 +1,23 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { userData } from '../interfaces/loginCredentials';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserValidationService {
+  url: string = 'https://jsonplaceholder.typicode.com/users';
 
+  private http: HttpClient = inject(HttpClient);
   constructor() { }
+
+  // this method will check if the login credentials are vald or not
+  checkLoginCredentials(username: string, password: string) {
+    return this.http.get<userData>(this.url).subscribe( (data) => {
+      data.find( (user) => {
+        return user.username === username && user.name === password;
+      })
+    });
+  }
+
 }
