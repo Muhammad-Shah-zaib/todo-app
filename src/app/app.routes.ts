4825adaf8b144cf94e1a1d5e530b431065c098da
Muhammd-Shah-zaib <muhammdshahzaib@outlook.com>
2024-01-20 16:12:43 +0500

    added routes for login and signup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { LoginComponent } from './pages/login/login.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent},
+    { path: 'signup', component: SignUpComponent},
     { path: 'home', redirectTo: '' },
     { path: 'todos', redirectTo: '' },
     { path: 'alltodos', redirectTo: '' },
