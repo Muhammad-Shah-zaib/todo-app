5471a8dba120a9a30a051cfd251e31ebccaf33bc
Muhammd-Shah-zaib <muhammdshahzaib@outlook.com>
2024-01-20 16:32:43 +0500

    added html and css for login page

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormField } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatFormField, MatInputModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
